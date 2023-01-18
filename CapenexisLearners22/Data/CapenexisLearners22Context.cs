34ea04ba1b451545a00dafb863f1d4512d08cbe9
MAMAOLWETHU <thulisiweb04@gmail.com>
2023-01-18 14:14:40 +0200

    Add project files.

---
+++ CapenexisLearners22/Data/CapenexisLearners22Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CapenexisLearners22;
+
+namespace CapenexisLearners22.Data
+{
+    public class CapenexisLearners22Context : DbContext
+    {
+        public CapenexisLearners22Context (DbContextOptions<CapenexisLearners22Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CapenexisLearners22.Learner> Learner { get; set; } = default!;
+
+        public DbSet<CapenexisLearners22.Facilitator> Facilitator { get; set; } = default!;
+
+        public DbSet<CapenexisLearners22.Course> Course { get; set; } = default!;
+    }
+}
---
+++ CapenexisLearners22/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CapenexisLearners22;
+
+public class Course
+{
+    public int Id { get; set; }
+    public string? Lecture { get; set; }
+    public string? CourseName { get; set; }
+    public string? Duration { get; set; }
+    public decimal Price { get; set; }
+}
